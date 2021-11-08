fad27d2cf84446c40739510285226d32e009ba3a
jung-jinwoo <wlsdn960319@gmail.com>
2021-11-08 12:25:33 +0900

    js basic # 12

---
+++ javascript-practice/basics/ex11.js
@@ -0,0 +1,59 @@
+/**
+    Array 메소드
+ */
+
+var colors = ['black', 'white', 'yellow'];
+var fruits = ['apple', 'mango', 'banana'];
+
+// concat
+var a1 = fruits.concat(colors);
+console.log(a1);
+
+// pop, push, stack 지원
+var color = colors.pop();
+console.log(color);
+console.log(colors);
+
+colors.push('red');
+console.log(colors);
+
+// join
+var str = fruits.join(":");
+console.log(str);
+
+// reverse
+console.log(fruits);
+fruits.reverse();
+console.log(fruits);
+
+// shift
+var numbers = [1000,3000,2000,5000,4000, 8000];
+var number = numbers.shift();
+console.log(number, numbers);
+
+// sort
+numbers.sort();
+console.log(numbers);
+
+//slice
+var numbers = numbers.slice(1, 3);
+console.log(numbers);
+
+// splice
+// index 부터 count개를 삭제후 삭제된 요소를 반환
+console.log(fruits);
+var fruits2 = fruits.splice(0/*index*/, 2/*count */);
+console.log(fruits, fruits2);
+
+var a1 = [0,1,2,3,4];
+var a2 = a1.splice(1, 3, 10);
+console.log(a1, a2);
+
+var a1 = [0,1,2,3,4];
+var a2 = a1.splice(1, 1, 10);
+console.log(a1, a2);
+
+var a1 = [0,1,2,3,4];
+var a2 = a1.splice(1, 0, 10);
+console.log(a1, a2);
+
